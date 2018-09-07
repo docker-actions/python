d9e18e41bffcccca81c893f20f7ea8341bf6143a
Ilja Bobkevic <ilja@spotify.com>
2018-09-07 21:45:54 +0200

    Bump bash container version
    Also switch to travis env instead of versions

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,21 +5,17 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for params in $(< versions.txt); do
-  arr_param=(${params//,/ })
-  build_args=''
-  for build_arg in ${arr_param[@]}; do
-    build_args="${build_args} --build-arg ${build_arg}"
-    eval "export ${build_arg}"
-  done
-
-  echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec python${MAJOR_VERSION}.${MINOR_VERSION} \"\$@\"" > entrypoint.sh
-
-  image_name=''
-  if [ "x${tag}" = "xlatest" ]; then
-    image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}-${tag}"
-  else
-    image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}-${tag}"
-  fi
-  docker build ${build_args} -t ${image_name} .
+build_args=''
+for ba in $BUILD_ARGS; do
+  build_args="${build_args} --build-arg ${ba}=${!ba}"
 done
+
+echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec python${MAJOR_VERSION}.${MINOR_VERSION} \"\$@\"" > entrypoint.sh
+
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}-${tag}"
+else
+  image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}-${tag}"
+fi
+docker build ${build_args} -t ${image_name} .
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,18 +5,11 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for params in $(< versions.txt); do
-  arr_param=(${params//,/ })
-  for build_arg in ${arr_param[@]}; do
-    eval "export ${build_arg}"
-  done
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}-${tag}"
+else
+  image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}-${tag}"
+fi
 
-  image_name=''
-  if [ "x${tag}" = "xlatest" ]; then
-    image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}-${tag}"
-  else
-    image_name="${docker_org}/python:${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}-${tag}"
-  fi
-
-  docker push ${image_name}
-done
+docker push ${image_name}
